4417546c85a1439ab557ce1529b84bb238ac9c86
Michal Vala <michal.vala@gmail.com>
2018-01-09 06:22:40 +0100

    init dependencies, styles and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './dynamic/header/header.component';
+import { MenuComponent } from './dynamic/menu/menu.component';
+import { TextComponent } from './dynamic/text/text.component';
+import { FooterComponent } from './dynamic/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainComponent,
+    HeaderComponent,
+    MenuComponent,
+    TextComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/page.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PageService {
+
+  components: Array<string> = [];
+
+  constructor() { }
+
+}
